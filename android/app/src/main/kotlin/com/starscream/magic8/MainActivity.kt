158f2a3237bcf924a4e2e39e041b30e09177a12a
kingpush37 <57881553+kingpush37@users.noreply.github.com>
2020-06-13 08:09:20 -0700

    initial commit

---
+++ android/app/src/main/kotlin/com/starscream/magic8/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.starscream.magic8
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: BallPage(),
+    )
+  );
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
