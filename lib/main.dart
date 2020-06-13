6ff95dc064cd9f36c1a910d920cabc109b35ffa9
kingpush37 <57881553+kingpush37@users.noreply.github.com>
2020-06-13 09:05:48 -0700

    ui interactive

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(
@@ -30,16 +31,20 @@ class Ball extends StatefulWidget {
 }
 
 class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Container(
         child: FlatButton(
             onPressed: () {
-              print('I got clicked!');
+              setState(() {
+              ballNumber = Random().nextInt(5) + 1;
+              });
             },
             child: Image.asset(
-                'images/ball1.png'
+                'images/ball$ballNumber.png'
             ),
         ),
       ),
