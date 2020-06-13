cecc67c0de6e882efb6966a6651f8ed9d777efbe
kingpush37 <57881553+kingpush37@users.noreply.github.com>
2020-06-13 08:39:35 -0700

    created widget UI

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,41 @@ void main() {
 class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          backgroundColor: Colors.lightBlue,
+          title: Text('Ask Me Anything'),
+        ),
+        body: Ball(),
+      ),
+    );
   }
 }
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: FlatButton(
+            onPressed: () {
+              print('I got clicked!');
+            },
+            child: Image.asset(
+                'images/ball1.png'
+            ),
+        ),
+      ),
+    );
+  }
+}
+
 
 
